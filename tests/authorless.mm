566680c0e20c0f02aa46107a88b54709831ba9a3
Joseph Reagle <596814+reagle@users.noreply.github.com>
2013-09-11 16:46:41 -0400

    new tests

---
+++ tests/authorless.mm
@@ -0,0 +1,17 @@
+<map version="0.7.2">
+<node TEXT="test">
+<node TEXT="foo" POSITION="right">
+<node COLOR="#338800" TEXT="Some Org">
+<node COLOR="#090f6b" TEXT="Title one">
+<node COLOR="#ff33b8" TEXT="y=1995 or=Some Org"/>
+</node>
+</node>
+<node COLOR="#338800" TEXT="Bob Turner">
+<node COLOR="#090f6b" TEXT="Title two">
+<node COLOR="#ff33b8" TEXT="y=1996 or=A6"/>
+</node>
+</node>
+</node>
+<node TEXT="pass" POSITION="left"/>
+</node>
+</map>

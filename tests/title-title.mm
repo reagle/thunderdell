9c01a833f4d88d43630280046b2ec428145d65b0
Joseph Reagle <joseph.2011@reagle.org>
2013-10-21 14:54:07 -0400

    clean up title-title case

--- tests/title-title.mm
+++ tests/title-title.mm
@@ -1,10 +1,10 @@
 <map version="0.7.2">
 <node TEXT="test">
 <node TEXT="pass" POSITION="left"/>
-<node COLOR="#338800" TEXT="A5" POSITION="right">
-<node COLOR="#090f6b" TEXT="Title five">
+<node COLOR="#338800" TEXT="A1" POSITION="right">
+<node COLOR="#090f6b" TEXT="Title one">
 <node COLOR="#ff33b8" TEXT="y=1995"/>
-<node COLOR="#090f6b" TEXT="Title six">
+<node COLOR="#090f6b" TEXT="Title two">
 <node COLOR="#ff33b8" TEXT="y=1996"/>
 </node>
 <node COLOR="#090f6b" TEXT="Title silly">
@@ -13,12 +13,15 @@
 <node COLOR="#090f6b" TEXT="Title sad">
 <node COLOR="#ff33b8" TEXT="y=1996"/>
 </node>
-<node COLOR="#090f6b" TEXT="Title seven">
+<node COLOR="#090f6b" TEXT="Title sextet">
 <node COLOR="#ff33b8" TEXT="y=1997"/>
 </node>
 </node>
 <node COLOR="#090f6b" TEXT="The title&apos;s apostrophes">
 <node COLOR="#ff33b8" TEXT="y=1997"/>
+</node>
+<node COLOR="#090f6b" TEXT="The title with a great-hypen">
+<node COLOR="#ff33b8" TEXT="y=1998"/>
 </node>
 </node>
 </node>

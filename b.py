fd4503d5cdc85b9000d8702768044dce7c8e63ec
Joseph Reagle <joseph.2011@reagle.org>
2015-07-10 16:20:59 -0400

    author flatten

--- b.py
+++ b.py
@@ -532,8 +532,12 @@ class scrape_DOI(scrape_default):
         if 'author' in bib_dict:
             names = ''
             for name_dic in bib_dict['author']:
-                info("name_dic.values() = %s" % name_dic.values())
-                names = names + ', ' + ' '.join(name_dic.values())
+                joined_name = ' '.join(
+                    [item for item in name_dic.values() if item])
+                info("joined_name = '%s'" %joined_name)
+                # info("name_dic.values() = %s" % name_dic.values())
+                # names = names + ', ' + ' '.join(name_dic.values())
+                names = names + ', ' + joined_name
             names = names[2:] # remove first comma
         return names
 

bc4509c6b415393800685f2e99c42aa0615f88ac
Joseph Reagle <596814+reagle@users.noreply.github.com>
2016-12-08 16:07:53 -0500

    new tests for freeplane transition

--- tests/editors.mm
+++ tests/editors.mm
@@ -1,17 +1,96 @@
-<map version="0.7.2">
-<node TEXT="test">
-<node TEXT="foo" POSITION="right">
-<node COLOR="#338800" TEXT="A1">
-<node COLOR="#090f6b" TEXT="Title one">
-<node COLOR="#ff33b8" TEXT="y=1995 e=John Smith, Sally Sue bt=Book One"/>
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="test" FOLDED="false" ID="ID_232484680"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="foo" POSITION="right" ID="ID_200850748">
+<node TEXT="A1" STYLE_REF="author" ID="ID_1325102143" COLOR="#338800">
+<node TEXT="Title one" STYLE_REF="cite" ID="ID_1553115971" COLOR="#090f6b">
+<node TEXT="y=1995 e=John Smith, Sally Sue bt=Book One" STYLE_REF="cite" ID="ID_745166565" COLOR="#ff33b8"/>
 </node>
 </node>
-<node COLOR="#338800" TEXT="A2">
-<node COLOR="#090f6b" TEXT="Title two">
-<node COLOR="#ff33b8" TEXT="y=1995 e=Jim Jorn and Kathy Kierra  bt=Book Two"/>
+<node TEXT="A2" STYLE_REF="author" ID="ID_1691648740" COLOR="#338800">
+<node TEXT="Title two" STYLE_REF="cite" ID="ID_443410096" COLOR="#090f6b">
+<node TEXT="y=1995 e=Jim Jorn and Kathy Kierra  bt=Book Two" STYLE_REF="cite" ID="ID_660100022" COLOR="#ff33b8"/>
 </node>
 </node>
 </node>
-<node TEXT="pass" POSITION="left"/>
+<node TEXT="pass" POSITION="left" ID="ID_788961397"/>
 </node>
 </map>
--- tests/title-title.mm
+++ tests/title-title.mm
@@ -1,27 +1,106 @@
-<map version="0.7.2">
-<node TEXT="test">
-<node TEXT="pass" POSITION="left"/>
-<node COLOR="#338800" TEXT="A1" POSITION="right">
-<node COLOR="#090f6b" TEXT="Title one">
-<node COLOR="#ff33b8" TEXT="y=1995"/>
-<node COLOR="#090f6b" TEXT="Title two">
-<node COLOR="#ff33b8" TEXT="y=1996"/>
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="test" FOLDED="false" ID="ID_1820743650"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="pass" POSITION="left" ID="ID_1487071178"/>
+<node TEXT="A1" STYLE_REF="author" POSITION="right" ID="ID_41986915" COLOR="#338800">
+<node TEXT="Title one" STYLE_REF="title" ID="ID_1733529255" COLOR="#090f6b">
+<node TEXT="y=1995" STYLE_REF="cite" ID="ID_1244474181" COLOR="#ff33b8"/>
+<node TEXT="Title two" STYLE_REF="title" ID="ID_1389838189" COLOR="#090f6b">
+<node TEXT="y=1996" STYLE_REF="cite" ID="ID_1046366542" COLOR="#ff33b8"/>
 </node>
-<node COLOR="#090f6b" TEXT="Title silly">
-<node COLOR="#ff33b8" TEXT="y=1996"/>
+<node TEXT="Title silly" STYLE_REF="title" ID="ID_1828390752" COLOR="#090f6b">
+<node TEXT="y=1996" STYLE_REF="cite" ID="ID_228653156" COLOR="#ff33b8"/>
 </node>
-<node COLOR="#090f6b" TEXT="Title sad">
-<node COLOR="#ff33b8" TEXT="y=1996"/>
+<node TEXT="Title sad" STYLE_REF="title" ID="ID_392988688" COLOR="#090f6b">
+<node TEXT="y=1996" STYLE_REF="cite" ID="ID_1951077131" COLOR="#ff33b8"/>
 </node>
-<node COLOR="#090f6b" TEXT="Title sextet">
-<node COLOR="#ff33b8" TEXT="y=1997"/>
+<node TEXT="Title sextet" STYLE_REF="title" ID="ID_540848705" COLOR="#090f6b">
+<node TEXT="y=1997" STYLE_REF="cite" ID="ID_1090608792" COLOR="#ff33b8"/>
 </node>
 </node>
-<node COLOR="#090f6b" TEXT="The title&apos;s apostrophes">
-<node COLOR="#ff33b8" TEXT="y=1997"/>
+<node TEXT="The title&apos;s apostrophes" STYLE_REF="title" ID="ID_843625401" COLOR="#090f6b">
+<node TEXT="y=1997" STYLE_REF="cite" ID="ID_1137278027" COLOR="#ff33b8"/>
 </node>
-<node COLOR="#090f6b" TEXT="The title with a great-hypen">
-<node COLOR="#ff33b8" TEXT="y=1998"/>
+<node TEXT="The title with a great-hypen" STYLE_REF="title" ID="ID_1992142215" COLOR="#090f6b">
+<node TEXT="y=1998" STYLE_REF="cite" ID="ID_1533862653" COLOR="#ff33b8"/>
 </node>
 </node>
 </node>

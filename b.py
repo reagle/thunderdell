470d1c123f77339e05e90ab840ee20ee61aa7ecb
Joseph Reagle <596814+reagle@users.noreply.github.com>
2017-03-30 12:43:39 -0400

    print date at console

--- b.py
+++ b.py
@@ -1193,8 +1193,8 @@ def do_console_annotation(biblio):
 
     info("biblio['author'] = '%s'" %(biblio['author']))
     tentative_id = get_tentative_ident(biblio)
-    print('''@%s : au=%s ti=%s\n''' % (tentative_id, 
-                    biblio['author'], biblio['title'])),
+    print('''@%s : au=%s ti=%s d=%s\n''' % (tentative_id, biblio['author'], 
+    	biblio['title'], biblio['date'])),
     for key in biblio:
         if key.startswith('c_'):
             print("    %s=%s" %(fe.CSL_FIELDS[key], biblio[key]))

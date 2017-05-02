e375e68207a3ce6a5e6fe22ce34b9cc0733085e7
Joseph Reagle <596814+reagle@users.noreply.github.com>
2017-05-02 07:48:14 -0400

    all checked in files now pep8 linted

--- doi_query.py
+++ doi_query.py
@@ -6,7 +6,7 @@
 # (c) Copyright 2015-2017 by Joseph Reagle
 # Licensed under the GPLv3, see <http://www.gnu.org/licenses/gpl-3.0.html>
 #
-''' Return CrossRef bibliographic data for a given a DOI. 
+''' Return CrossRef bibliographic data for a given a DOI.
     See http://www.crossref.org/CrossTech/2011/11/turning_dois_into_formatted_ci.html
 '''
 
@@ -16,28 +16,29 @@ import pprint
 import requests
 import sys
 
-log_level = 100 # default
+log_level = 100  # default
 critical = logging.critical
 info = logging.info
 dbg = logging.debug
 
 ACCEPT_HEADERS = {
-    'json' : 'application/citeproc+json',
-    'bibtex' : 'text/bibliography;style=bibtex',
-    }
+    'json': 'application/citeproc+json',
+    'bibtex': 'text/bibliography;style=bibtex', }
+
 
 def query(doi, accept='application/citeproc+json'):
     """Query the DOI Web service; returns string"""
-    
+
     info('accept = %s ' % accept)
     info("doi = %s" % doi)
     headers = {'Accept': accept}
-    url = "http://dx.doi.org/%s" %doi
-    info("url = '%s'" %url)
+    url = "http://dx.doi.org/%s" % doi
+    info("url = '%s'" % url)
     r = requests.get(url, headers=headers)
     requested_content_type = accept.split(';')[0]
     returned_content_type = r.headers['content-type']
-    info("returned_content_type = '%s'; requested_content_type = '%s'" %(returned_content_type, requested_content_type))
+    info("returned_content_type = '%s'; requested_content_type = '%s'"
+         % (returned_content_type, requested_content_type))
     if requested_content_type in returned_content_type:
         json_bib = json.loads(r.content)
         return(json_bib)
@@ -46,18 +47,21 @@ def query(doi, accept='application/citeproc+json'):
 
 if '__main__' == __name__:
 
-    import argparse 
+    import argparse
     arg_parser = argparse.ArgumentParser(
         description='Given a doi return bibliographic data.')
     # positional arguments
     arg_parser.add_argument('DOI', nargs='+')
     # optional arguments
-    arg_parser.add_argument("-s", "--style",
+    arg_parser.add_argument(
+        "-s", "--style",
         help="style of bibliography data")
-    arg_parser.add_argument('-l', '--log-to-file',
+    arg_parser.add_argument(
+        '-l', '--log-to-file',
         action="store_true", default=False,
         help="log to file %(prog)s.log")
-    arg_parser.add_argument('-V', '--verbose', action='count', default=0,
+    arg_parser.add_argument(
+        '-V', '--verbose', action='count', default=0,
         help="Increase verbosity (specify multiple times for more)")
     arg_parser.add_argument('--version', action='version', version='0.1')
     args = arg_parser.parse_args()
@@ -68,16 +72,16 @@ if '__main__' == __name__:
     LOG_FORMAT = "%(levelno)s %(funcName).5s: %(message)s"
     if args.log_to_file:
         logging.basicConfig(filename='doi_query.log', filemode='w',
-            level=log_level, format = LOG_FORMAT)
+                            level=log_level, format=LOG_FORMAT)
     else:
-        logging.basicConfig(level=log_level, format = LOG_FORMAT)
+        logging.basicConfig(level=log_level, format=LOG_FORMAT)
 
-    accept = ACCEPT_HEADERS['json'] 
+    accept = ACCEPT_HEADERS['json']
     if args.style:
         if args.style in ACCEPT_HEADERS:
             accept = ACCEPT_HEADERS[args.style]
         else:
-            accept = args.style   
+            accept = args.style
     info('accept = %s ' % accept)
-    
+
     pprint.pprint(query(args.DOI[0], accept))

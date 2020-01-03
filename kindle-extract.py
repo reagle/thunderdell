9b7c3b514552303e83bacd501559437b1a1df79a
Joseph Reagle <596814+reagle@users.noreply.github.com>
2020-01-03 09:57:34 -0500

    initial pseudo-code for kindle-extract

---
+++ kindle-extract.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This file is part of Thunderdell/BusySponge
+# <http://reagle.org/joseph/2009/01/thunderdell>
+# (c) Copyright 2009-2016 by Joseph Reagle
+# Licensed under the GPLv3, see <http://www.gnu.org/licenses/gpl-3.0.html>
+#
+
+"""extract a MM from Kindle notes
+    - Using device, CSV can be emailed
+    - From Kindle Cloud, copy/paste into text file
+"""
+
+parse CSV
+first line = 
+second line = 
+third line = Amazon URL
+pagination = location
+edition = Kindle
+for each subsequent line
+    if 'highlight' in column 1:
+        color =         
+    elif 'note' in column 1:
+        color =
+    else
+        unknown type
+    location = 
+    text = 
+    for line in text.split('\t'):
+        if line is short:
+            its a section
+        else:
+            its a line
+

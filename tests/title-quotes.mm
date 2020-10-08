cf48bb0ef7310b114ba878a2c23464b7b9a812ea
reagle <joseph.acct@reagle.org>
2020-10-08 13:12:33 -0400

    tweak and tests for nested quotes

---
+++ tests/title-quotes.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="title escapes" FOLDED="false" ID="ID_334406316"><hook NAME="MapStyle">
+    <properties show_note_icons="true" fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork" MAX_WIDTH="15.0 cm">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="author" COLOR="#338800"/>
+<stylenode TEXT="title" COLOR="#090f6b"/>
+<stylenode TEXT="cite" COLOR="#ff33b8"/>
+<stylenode TEXT="annotation" COLOR="#999999"/>
+<stylenode TEXT="quote" COLOR="#166799"/>
+<stylenode TEXT="paraphrase" COLOR="#8b12d6"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Joseph Reagle" STYLE_REF="author" POSITION="right" ID="ID_1484729955" COLOR="#338800">
+<node TEXT="&quot;A quote with &apos;square quotes&apos;&quot;: Can&apos;t believe this ugly title" STYLE_REF="title" ID="ID_1278536345" LINK="https://firstmonday.org/article/view/4291/3381" COLOR="#090f6b">
+<node TEXT="or=test.org d=20191008 r=20201008 11:08 UTC kw=test" STYLE_REF="cite" ID="ID_1210073182" COLOR="#ff33b8"/>
+<node TEXT="Hopefully this can handle the nesting" STYLE_REF="annotation" ID="ID_1659017802"/>
+</node>
+<node TEXT="&quot;A quote with &apos;square quotes&apos;&quot;: Can&apos;t believe this ugly title" STYLE_REF="title" ID="ID_365937880" LINK="https://firstmonday.org/article/view/4291/3381" COLOR="#090f6b">
+<node TEXT="or=test.org d=20201008 r=20201008 11:08 UTC kw=test" STYLE_REF="cite" ID="ID_1924998463" COLOR="#ff33b8"/>
+<node TEXT="Hopefully this can handle the nesting" STYLE_REF="annotation" ID="ID_1931103454"/>
+</node>
+</node>
+</node>
+</map>

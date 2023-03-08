ba27675e44a5df3f5e82a733d83ef6afca3da6c0
Joseph Reagle <596814+reagle@users.noreply.github.com>
2023-03-08 10:38:28 -0500

    merging reordering of functions

--- utils/web.py
+++ utils/web.py
@@ -27,42 +27,6 @@ info = logging.info
 dbg = logging.debug
 
 
-def escape_XML(s):  # http://wiki.python.org/moin/EscapingXml
-    """Escape XML character entities; & < > are defaulted"""
-    extras = {"\t": "  "}
-    return escape(s, extras)
-
-
-def unescape_XML(text):  # .0937s 4.11%
-    """
-    Removes HTML or XML character references and entities from text.
-    http://effbot.org/zone/re-sub.htm#unescape-htmlentitydefs
-    Marginally faster than `from xml.sax.saxutils import escape, unescape`
-
-    """
-
-    def fixup_chars(m):
-        text = m.group(0)
-        if text[:2] == "&#":
-            # character reference
-            try:
-                if text[:3] == "&#x":
-                    return chr(int(text[3:-1], 16))
-                else:
-                    return chr(int(text[2:-1]))
-            except ValueError:
-                pass
-        else:
-            # named entity
-            try:
-                text = chr(html.entities.name2codepoint[text[1:-1]])
-            except KeyError:
-                pass
-        return text  # leave as is
-
-    return re.sub(r"&#?\w+;", fixup_chars, text)
-
-
 def get_HTML(
     url,
     referer="",
@@ -127,69 +91,46 @@ def get_text(url: str) -> str:
     return str(os.popen(f'w3m -O utf8 -cols 10000 -dump "{url}"').read())
 
 
-def len_twitter(text: str) -> int:
-    """
-    Twitter counts code units not code points as part of its
-    character limit.
-
-    https://developer.twitter.com/en/docs/counting-characters
-
-    """
-
-    return len(text.encode("utf-16-le")) // 2
-
-
-def shrink_message(service, comment, title, url, tags):
-    """Shrink message to fit into limit"""
-
-    if service == "ohai":  # mastodon instance
-        limit = 500
-    elif service == "twitter":
-        limit = 280
-    info(f"{comment=}")
-    PADDING = 7  # = comment_delim + title quotes + spaces
-    TWITTER_SHORTENER_LEN = 23  # twitter uses t.co
-    limit = limit - PADDING
-
-    info(f"{limit=}")
-    message_room = limit - len_twitter(tags)
-    info(f"message_room - len(tags) = {message_room}")
-
-    info(f"{len_twitter(url)=}")
-    if service == "twitter" and len_twitter(url) > TWITTER_SHORTENER_LEN:
-        message_room = message_room - TWITTER_SHORTENER_LEN
-        info(f"  shortened to {TWITTER_SHORTENER_LEN}")
+def yasn_publish(comment, title, subtitle, url, tags):
+    "Send annotated URL to social networks"
+    info(f"'{comment=}', {title=}, {subtitle=}, {url=}, {tags=}")
+    if tags and tags[0] != "#":  # they've not yet been hashified
+        tags = " ".join(
+            ["#" + KEY_SHORTCUTS.get(tag, tag) for tag in tags.strip().split(" ")]
+        )
+    comment, title, subtitle, url, tags = (
+        v.strip() if isinstance(v, str) else ""
+        for v in [comment, title, subtitle, url, tags]
+    )
+    if subtitle:
+        title = f"{title}: {subtitle}"
+    if url[-4:] in (".jpg", ".png"):
+        if "goatee.net/photo" in url:
+            title = ""
+            tags = "#photo #" + url.rsplit("/")[-1][8:-4].replace("-", " #")
+            photo_path = Path(f"{config.HOME}/f/{url[19:]}")
+        if url.startswith("file://"):
+            url.replace("file:///", "file://")
+            title = ""
+            tags = "#image"
+            photo_path = Path(url.split("//", 1)[1])
+            if not photo_path.exists():
+                raise OSError(f"{photo_path} doesn't exist.")
     else:
-        message_room = message_room - len_twitter(url)
-    info(f"message_room after url = {message_room}")
+        photo_path = None
+    if url.startswith("file://"):
+        url = ""
+    total_len = len(comment) + len(tags) + len(title) + len(url)
+    info(
+        f"""comment = {len(comment)}: {comment}
+         title = {len(title)}: {title}
+         url = {len(url)}: {url}
+         tags = {len(tags)}: {tags}
+         {total_len=}"""
+    )
 
-    info(f"{len_twitter(title)=}")
-    if len_twitter(title) > message_room:
-        info("title is too long")
-        title = f"{title[:message_room - 1]}…"
-        info(f"  truncated to {len_twitter(title)}")
-    message_room = message_room - len_twitter(title)
-    info(f"{message_room=} after title = ")
-
-    info(f"{len_twitter(comment)=}")
-    if len_twitter(comment) > message_room:
-        info("comment is too long")
-        if message_room > 5:
-            info(" truncating")
-            comment = f"{comment[:message_room - 1]}…"
-            info(f"  truncated to {len_twitter(comment)}")
-            info(f"{comment}")
-        else:
-            info(" skipping")
-            comment = ""
-    message_room = message_room - len_twitter(comment)
-    info(f"message_room after comment = {message_room}")
-
-    comment_delim = ": " if comment and title else ""
-    title = f"“{title}”" if title else ""
-    message = f"{comment}{comment_delim}{title} {url} {tags}".strip()
-    critical(f"{len_twitter(message)=}: {message=}")
-    return message
+    twitter_update(comment, title, url, tags, photo_path)
+    mastodon_update(comment, title, url, tags, photo_path)
 
 
 def twitter_update(
@@ -253,43 +194,102 @@ def mastodon_update(
         print(f"toot worked {len(toot)}: {toot}")
 
 
-def yasn_publish(comment, title, subtitle, url, tags):
-    "Send annotated URL to social networks"
-    info(f"'{comment=}', {title=}, {subtitle=}, {url=}, {tags=}")
-    if tags and tags[0] != "#":  # they've not yet been hashified
-        tags = " ".join(
-            ["#" + KEY_SHORTCUTS.get(tag, tag) for tag in tags.strip().split(" ")]
-        )
-    comment, title, subtitle, url, tags = (
-        v.strip() if isinstance(v, str) else ""
-        for v in [comment, title, subtitle, url, tags]
-    )
-    if subtitle:
-        title = f"{title}: {subtitle}"
-    if url[-4:] in (".jpg", ".png"):
-        if "goatee.net/photo" in url:
-            title = ""
-            tags = "#photo #" + url.rsplit("/")[-1][8:-4].replace("-", " #")
-            photo_path = Path(f"{config.HOME}/f/{url[19:]}")
-        if url.startswith("file://"):
-            url.replace("file:///", "file://")
-            title = ""
-            tags = "#image"
-            photo_path = Path(url.split("//", 1)[1])
-            if not photo_path.exists():
-                raise OSError(f"{photo_path} doesn't exist.")
-    else:
-        photo_path = None
-    if url.startswith("file://"):
-        url = ""
-    total_len = len(comment) + len(tags) + len(title) + len(url)
-    info(
-        f"""comment = {len(comment)}: {comment}
-         title = {len(title)}: {title}
-         url = {len(url)}: {url}
-         tags = {len(tags)}: {tags}
-         {total_len=}"""
-    )
+def shrink_message(service, comment, title, url, tags):
+    """Shrink message to fit into limit"""
 
-    twitter_update(comment, title, url, tags, photo_path)
-    mastodon_update(comment, title, url, tags, photo_path)
+    if service == "ohai":  # mastodon instance
+        limit = 500
+    elif service == "twitter":
+        limit = 280
+    info(f"{comment=}")
+    PADDING = 7  # = comment_delim + title quotes + spaces
+    TWITTER_SHORTENER_LEN = 23  # twitter uses t.co
+    limit = limit - PADDING
+
+    info(f"{limit=}")
+    message_room = limit - len_twitter(tags)
+    info(f"message_room - len(tags) = {message_room}")
+
+    info(f"{len_twitter(url)=}")
+    if service == "twitter" and len_twitter(url) > TWITTER_SHORTENER_LEN:
+        message_room = message_room - TWITTER_SHORTENER_LEN
+        info(f"  shortened to {TWITTER_SHORTENER_LEN}")
+    else:
+        message_room = message_room - len_twitter(url)
+    info(f"message_room after url = {message_room}")
+
+    info(f"{len_twitter(title)=}")
+    if len_twitter(title) > message_room:
+        info("title is too long")
+        title = f"{title[:message_room - 1]}…"
+        info(f"  truncated to {len_twitter(title)}")
+    message_room = message_room - len_twitter(title)
+    info(f"{message_room=} after title = ")
+
+    info(f"{len_twitter(comment)=}")
+    if len_twitter(comment) > message_room:
+        info("comment is too long")
+        if message_room > 5:
+            info(" truncating")
+            comment = f"{comment[:message_room - 1]}…"
+            info(f"  truncated to {len_twitter(comment)}")
+            info(f"{comment}")
+        else:
+            info(" skipping")
+            comment = ""
+    message_room = message_room - len_twitter(comment)
+    info(f"message_room after comment = {message_room}")
+
+    comment_delim = ": " if comment and title else ""
+    title = f"“{title}”" if title else ""
+    message = f"{comment}{comment_delim}{title} {url} {tags}".strip()
+    info(f"{len_twitter(message)=}: {message=}")
+    return message
+
+
+def len_twitter(text: str) -> int:
+    """
+    Twitter counts code units not code points as part of its
+    character limit.
+
+    https://developer.twitter.com/en/docs/counting-characters
+
+    """
+
+    return len(text.encode("utf-16-le")) // 2
+
+
+def escape_XML(s):  # http://wiki.python.org/moin/EscapingXml
+    """Escape XML character entities; & < > are defaulted"""
+    extras = {"\t": "  "}
+    return escape(s, extras)
+
+
+def unescape_XML(text):  # .0937s 4.11%
+    """
+    Removes HTML or XML character references and entities from text.
+    http://effbot.org/zone/re-sub.htm#unescape-htmlentitydefs
+    Marginally faster than `from xml.sax.saxutils import escape, unescape`
+
+    """
+
+    def fixup_chars(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return chr(int(text[3:-1], 16))
+                else:
+                    return chr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = chr(html.entities.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text  # leave as is
+
+    return re.sub(r"&#?\w+;", fixup_chars, text)

b48a40d773ec2f81d5a4e0381fd5750cf1f947ab
Joseph Reagle <596814+reagle@users.noreply.github.com>
2023-05-09 18:02:09 -0400

    move to pytest for testing

---
+++ tests/test_extract_kindle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# This file is part of Thunderdell/BusySponge
+# <https://reagle.org/joseph/2009/01/thunderdell>
+# (c) Copyright 2009-2023 by Joseph Reagle
+# Licensed under the GPLv3, see <http://www.gnu.org/licenses/gpl-3.0.html>
+#
+"""
+Run tests against golden YAML results; 
+useful for detecting inadvertent changes.
+"""
+from config import TESTS_FOLDER
+from extract_kindle import process_html  # parse_args
+
+
+def test_process_html():
+    """
+    Tests the processing of a Kindle HTML export.
+    """
+
+    # test_args = []
+    # args = parse_args(test_args)
+
+    given_fn = TESTS_FOLDER / "kindle-given.html"
+    given_txt = given_fn.read_text()
+
+    result_txt = process_html(given_txt)
+    # print(f"{result=}")
+
+    with open(TESTS_FOLDER / "kindle-result.txt", "w") as result_fd:
+        result_fd.write(result_txt)
+    with open(TESTS_FOLDER / "kindle-expected.txt") as expected_fd:
+        expected_txt = expected_fd.read()
+    # print(f"{expected=}")
+    assert result_txt == expected_txt
+
+
+if __name__ == "__main__":
+    test_process_html()

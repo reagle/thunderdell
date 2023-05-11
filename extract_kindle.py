6abb256dd427e121f7b6c684b61bcb528efdd089
Joseph Reagle <596814+reagle@users.noreply.github.com>
2023-05-11 09:47:12 -0400

    chore:linting/typing

--- extract_kindle.py
+++ extract_kindle.py
@@ -8,16 +8,15 @@ __license__ = "GLPv3"
 __version__ = "1.0"
 
 import argparse  # http://docs.python.org/dev/library/argparse.html
-import difflib
 import logging
 import re
 import subprocess
 import sys
 from email import policy
 from email.parser import BytesParser
-from os.path import splitext
+from pathlib import Path
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # type:ignore
 
 import change_case
 from utils.extract import get_bib_preamble
@@ -31,29 +30,30 @@ critical = logging.critical
 exception = logging.exception
 
 
-def process_email(file_name):
-    """Process parts of a MIME message store in file."""
+def process_email(file_name: Path) -> str:
+    """Process parts of a MIME message stored in file."""
 
     with open(file_name, "rb") as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
         for part in msg.walk():
-            debug(f"{part=}")
             msg_content_type = part.get_content_subtype()
+            debug(f"{part=}, {msg_content_type=}")
             if msg_content_type == "html":
                 debug("part is HTML: %s" % msg_content_type)
                 charset = part.get_content_charset(failobj="utf-8")
                 content = part.get_payload(decode=True).decode(charset, "replace")
                 return content
+        raise Exception("There's no HTML attachment to process.")
 
 
-def process_html(content):
+def process_html(content: str) -> str:
     """Process text for annotation kind, color, and page number."""
 
     RE_ISBN = re.compile(r"978(?:-?\d){10}")
 
     RE_COLOR_PAGE = re.compile(
         r"(?P<color>yellow|blue)</span>\) .*? (?P<type>Page|Location)"
-        r" (?P<page>[\dcdilmxv]+)",
+        + r" (?P<page>[\dcdilmxv]+)",
     )
     color = ""
     page = ""
@@ -90,7 +90,9 @@ def process_html(content):
     return "\n".join(text_new)
 
 
-def main(argv):
+def parse_args(argv: list) -> argparse.Namespace:
+    """Process arguments"""
+    # https://docs.python.org/3/library/argparse.html
     """Process arguments"""
     # https://docs.python.org/3/library/argparse.html
     arg_parser = argparse.ArgumentParser(
@@ -101,7 +103,7 @@ def main(argv):
     )
 
     # positional arguments
-    arg_parser.add_argument("file_names", nargs="*", metavar="FILE_NAMES")
+    arg_parser.add_argument("file_names", nargs="*", type=Path, metavar="FILE_NAMES")
     # optional arguments
     arg_parser.add_argument(
         "-o",
@@ -155,89 +157,44 @@ def main(argv):
     return args
 
 
-TEST_IN = """
-<html>
-<div class="noteText">
-    ISBN: 978-1-101-20085-8
-</div>
-<div class="noteText">
-    Men are rather reasoning than reasonable animals, for the most part governed by the impulse of passion.—LETTER OF APRIL 16, 1802
-</div><div class="sectionHeading">
-    PROLOGUE: The Oldest Revolutionary War Widow
-</div>
-<div class="noteHeading">
-    Highlight(<span class="highlight_yellow">yellow</span>) - Page 1 · Location 205
-</div>
-<div class="noteText">
-    PROLOGUE THE OLDEST REVOLUTIONARY WAR WIDOW
-</div><div class="noteHeading">
-    Highlight(<span class="highlight_yellow">yellow</span>) - Page 4 · Location 274
-</div>
-<div class="noteText">
-    He and James Madison were the prime movers behind the summoning of the Constitutional Convention and the chief authors of that classic gloss on the national charter, The Federalist, which Hamilton supervised.
-</div>
-
-</div><div class="noteHeading">
-    Highlight(<span class="highlight_yellow">yellow</span>) - 3. The Rise of the Big Black Woman > Page 88 · Location 1536
-</div>
-<div class="noteText">
-    in the context of the Enlightenment, the British preoccupation with the ills of excess feeding were folded into the racial discourse. This helped to make overindulgence evidence of not only slow wit but also barbarism.
-</div>
-
-</html>
-"""  # noqa: E501
-
-TEST_OUT = """author = Ron Chernow title = Alexander Hamilton date = 20050329 publisher = Penguin isbn = 9781101200858 url = https://books.google.com/books?isbn=9781101200858\n1 excerpt. PROLOGUE THE OLDEST REVOLUTIONARY WAR WIDOW\n4 excerpt. He and James Madison were the prime movers behind the summoning of the Constitutional Convention and the chief authors of that classic gloss on the national charter, The Federalist, which Hamilton supervised.\n88 excerpt. in the context of the Enlightenment, the British preoccupation with the ills of excess feeding were folded into the racial discourse. This helped to make overindulgence evidence of not only slow wit but also barbarism."""  # noqa: E501
-
-# TODO use '--' or some other method to indicate my thoughts (not
-#     paraphrase or quotes.
-
 if __name__ == "__main__":
-    args = main(sys.argv[1:])
+    args = parse_args(sys.argv[1:])
     info("==================================")
     debug(f"{args=}")
-    if args.test:
-        TEST_RESULTS = process_html(TEST_IN)
-        print("------------------------")
-        if repr(TEST_OUT) != repr(TEST_RESULTS):
-            print(f"\nSHOULD BE:\n```{repr(TEST_OUT)}```")
-            print(f"\nRESULT IS:\n```{repr(TEST_RESULTS)}```")
-            # debug(f"{type(TEST_OUT)=}", f"{len(TEST_OUT)=}")
-            # debug(f"{type(TEST_RESULTS)=}", f"{len(TEST_RESULTS)=}")
-            for diff in difflib.context_diff(
-                process_html(TEST_IN).split("\n"), TEST_OUT.split("\n")
-            ):
-                print(diff)
-        else:
-            print("tests pass")
-        sys.exit()
 
     file_names = args.file_names
     for file_name in file_names:
         debug(f"{file_name=}")
-        if file_name.endswith(".eml"):
-            fixed_fn = splitext(file_name)[0] + "-fixed.txt"
-            user_input = input("\npublish to social media? 'y' for yes: ")
-            if user_input == "y":
-                do_publish = "-p"
-            else:
-                do_publish = ""
-            cmd_extract_dication = ["extract_dictation.py", do_publish, fixed_fn]
-            content = process_email(file_name)
-            new_text = process_html(content)
-            if args.output_to_file:
-                with open(fixed_fn, "w") as fixed_fd:
-                    fixed_fd.write(new_text)
-                subprocess.run(["open", fixed_fn])
-                user_input = input(
-                    "\nfollow up with extract_dictation.py? 'y' for yes: "
-                )
-                if user_input == "y":
-                    subprocess.run(cmd_extract_dication)
-                print(f"{cmd_extract_dication}")
-            else:
-                print(new_text)
+        fixed_fn = file_name.with_stem(file_name.stem + "-fixed").with_suffix(".txt")
+        if file_name.suffix == ".eml":
+            info(f"processing {file_name} as eml")
+            html_content = process_email(file_name)
+        elif file_name.suffix == ".html":
+            info(f"processing {file_name} as html")
+            html_content = file_name.read_text()
         else:
             raise OSError(
                 "Do not recognize file type: {file_name} {splitext(file_name)[1]}."
             )
+
+        user_input = input(
+            "\nIn the output to come, include option for publish to social media? 'y'"
+            + " for yes: "
+        )
+        if user_input == "y":
+            do_publish = "-p"
+        else:
+            do_publish = ""
+
+        cmd_extract_dication = ["extract_dictation.py", do_publish, fixed_fn]
+        new_text = process_html(html_content)
+
+        if args.output_to_file:
+            fixed_fn.write_text(new_text)
+            subprocess.run(["open", str(fixed_fn)])
+            user_input = input("\nfollow up with extract_dictation.py? 'y' for yes: ")
+            if user_input == "y":
+                subprocess.run(cmd_extract_dication)
+            print(f"{cmd_extract_dication}")
+        else:
+            print(new_text)

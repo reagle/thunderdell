36ad0dd550acb8e0d56b486fe030df4734fd63e6
Joseph Reagle <596814+reagle@users.noreply.github.com>
2024-09-19 16:25:01 -0400

    add '$' to escaping test

--- tests/title-escapes.mm
+++ tests/title-escapes.mm
@@ -1,17 +1,21 @@
-<map version="freeplane 1.9.0">
-<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="title escapes" FOLDED="false" ID="ID_334406316"><hook NAME="MapStyle">
-    <properties fit_to_viewport="false;"/>
+    <properties fit_to_viewport="false;" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_1717264224" COLOR="#000000" STYLE="fork" MAX_WIDTH="15 cm">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1717264224" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
 <font SIZE="9"/>
 </stylenode>
@@ -20,9 +24,9 @@
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#ccffff" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#002080"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#ccffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#002080"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode TEXT="author" COLOR="#338800"/>
 <stylenode TEXT="title" COLOR="#090f6b"/>
 <stylenode TEXT="cite" COLOR="#ff33b8"/>
@@ -42,7 +46,7 @@
 <icon BUILTIN="yes"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
@@ -87,8 +91,8 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="Wikipedia" STYLE_REF="author" POSITION="right" ID="ID_1484729955" COLOR="#338800">
-<node TEXT="Bracket (e.g., [], {})" STYLE_REF="title" ID="ID_1278536345" LINK="http://en.wikipedia.org/w/index.php?title=Bracket&amp;oldid=295177316" COLOR="#090f6b">
+<node TEXT="Wikipedia" STYLE_REF="author" POSITION="bottom_or_right" ID="ID_1484729955" COLOR="#338800">
+<node TEXT="Bracket (e.g., [], {}, $$$)" STYLE_REF="title" ID="ID_1278536345" LINK="http://en.wikipedia.org/w/index.php?title=Bracket&amp;oldid=295177316" COLOR="#090f6b">
 <node TEXT="or=Wikipedia d=20090608 r=20090610 12:58 UTC kw=test" STYLE_REF="cite" ID="ID_1210073182" COLOR="#ff33b8"/>
 <node TEXT="Brackets are punctuation marks used in pairs to set apart or interject text within other text. In computer science, the term is sometimes said to strictly apply to the square or box type.[1]" STYLE_REF="quote" ID="ID_1659017802" COLOR="#166799"/>
 </node>

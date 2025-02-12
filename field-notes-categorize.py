6b146b4a2daee726f4bd42288b724cf641a31ce2
Joseph Reagle <596814+reagle@users.noreply.github.com>
2025-02-12 13:22:29 -0500

    fix: cleanup/update field-notes-categorize.py

--- field-notes-categorize.py
+++ field-notes-categorize.py
@@ -14,30 +14,35 @@ import lxml.etree as et
 
 
 def categorize_mindmap(old_fn: Path) -> None:
+    """Create a categorized mindmap based on the first `kw=` declaration in each source."""
     cat_fn = Path(old_fn.stem + "-cat.mm")
     old_doc = et.parse(old_fn)
     old_map = old_doc.getroot()
 
     categorized_nodes = {}
 
-    for old_node in old_map.getiterator():
+    for old_node in old_map.iter():
         if old_node.get("STYLE_REF", "default") == "cite":
-            print(f"{old_node.get('TEXT')}")
-            if "kw=" in old_node.get("TEXT"):
+            keyword = "UNLABELLED"
+            title_node = old_node.getparent()
+            assert title_node is not None, (
+                f"Title node missing for {old_node.get('TEXT', '')}"
+            )
+            author_node = title_node.getparent()
+            assert author_node is not None, (
+                f"Author node missing for {old_node.get('TEXT', '')}"
+            )
+            old_node_text = old_node.get("TEXT", "")
+            print(f"{old_node_text}")
+            if "kw=" in old_node_text:
                 try:
-                    kmatch = re.findall(r"kw=\S+", old_node.get("TEXT"), re.IGNORECASE)
+                    kmatch = re.findall(r"kw=\S+", old_node_text, re.IGNORECASE)
                     keyword = kmatch[0].split("kw=")[1]  # use first keyword
                 except IndexError as e:
-                    print(
-                        f"\nCouldn't find kw=__ value for {old_node.get('TEXT')}: {e=}"
-                    )
-            else:
-                keyword = "UNLABELLED"
-            categorized_nodes.setdefault(keyword, []).append(
-                old_node.getparent().getparent()
-            )
+                    print(f"\nCouldn't find kw=__ value for {old_node_text}: {e=}")
+            categorized_nodes.setdefault(keyword, []).append(author_node)
 
-    new_map = et.Element("map", version="1.10.3")
+    new_map = et.Element("map", version="freeplane 1.12.1")
     new_doc = et.ElementTree(new_map)
     root_node = et.SubElement(new_map, "node")
     root_node.set("TEXT", str(cat_fn))
